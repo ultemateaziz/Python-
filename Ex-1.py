12dc3aac5f9f1c5c0da64b08fedd379f466eacd8
ultemateaziz <71059002+ultemateaziz@users.noreply.github.com>
2022-10-21 17:25:19 +0530

    Add files via upload

---
+++ Ex-1.py
@@ -0,0 +1,4 @@
+for s in range(1,20):
+    {
+        print(s)        
+    }
---
+++ Ex-2.py
@@ -0,0 +1,11 @@
+# Function Definition & Function call
+  
+def evenOdd(x):
+    if (x % 2 == 0):
+        print("even")
+    else:
+        print("odd")
+  
+# Driver code to call the function
+evenOdd(123)
+evenOdd(30)
